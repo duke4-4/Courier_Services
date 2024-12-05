299dda6570e13600bcd782d24a02b7ad309f3997
Duke Chizengwe <dr.duke260@gmail.com>
2024-12-05 18:02:14 +0200

    Integrate Tailwind CSS forms plugin into configuration
    - Imported the forms plugin from '@tailwindcss/forms' for enhanced form styling.
    - Updated the plugins array to use the imported forms variable instead of requiring it directly.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -33,6 +35,6 @@ export default {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    forms
   ],
 } 
